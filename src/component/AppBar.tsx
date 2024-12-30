bb6a15f4adfe575e66557da54d4d0fdaf61cb193
Jatinn517 <jatinnagarwalofficial@gmail.com>
2024-12-30 10:59:03 +0530

    fixed bugs

--- src/component/AppBar.tsx
+++ src/component/AppBar.tsx
@@ -1,28 +1,8 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function AppBar() {
 
     const Navigate = useNavigate();
-    const [hideButton, setHideButton] = useState<boolean>(true);
-    const [hidebar, setHideBar] = useState<boolean>(false);
-
-    useEffect(() => {
-        function handleResponsiveness() {
-            if (window.innerWidth >= 768) {
-                setHideBar(false);
-                setHideButton(true);
-            }
-            else {
-                setHideBar(true);
-                setHideButton(false);
-            }
-        }
-        handleResponsiveness();
-        window.addEventListener('resize', handleResponsiveness);
-
-        return () => { window.removeEventListener('resize', handleResponsiveness); }
-    })
 
     function handleBlogsClick() {
         if (!sessionStorage.getItem('token')) {
@@ -31,11 +11,6 @@ export function AppBar() {
         }
         Navigate('/blogs');
     }
-
-    function showBar(){
-        setHideBar(c=>!c);
-    }
-
     return (
         <div className="flex justify-between items-center p-2 fixed top-0 left-0 w-full bg-white z-10 shadow-md">
             <button className="text-2xl font-bold" onClick={handleBlogsClick}>Blogs</button>
---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://server.jatinagarwal9669.workers.dev";
